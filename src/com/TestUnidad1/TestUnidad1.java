46ffa18f8338cae75b80b4377defc4363007c247
cflorest <cristian.jft84@gmail.com>
2022-10-17 22:09:15 -0300

    Desarrollo Ejercicio 1 Introducción Java

--- src/com/TestUnidad1/TestUnidad1.java
+++ src/com/TestUnidad1/TestUnidad1.java
@@ -3,6 +3,29 @@ package com.TestUnidad1;
 public class TestUnidad1
 {
     public static void main(String[] args) {
+    // Recordatorio: Los tipos de datos más comunes son int, long, double, boolean, String.
+
+        // 1.1 Enteros
+        byte variable1= 4;
+        short variable2 = 10;
+        int variable3 = 30;
+        long variable4 = 100;
+
+        // 1.2 Decimales
+        float variable5 = 5.5f;
+        double variable6 = 10.5d;
+
+        // 2 Boolean
+        boolean variable7 = false;
+
+        // Texto
+        char variable9 = 'a';
+        String variable10 = "lorem";
+
+        System.out.println("Enteros "+ variable1+", "+variable2+", "+variable3 + ", "+variable4);
+        System.out.println("Decimales "+variable5+", "+ variable6);
+        System.out.println("Boolean "+variable7);
+        System.out.println("Texto "+variable9+", "+variable10);
 
     }
 }
